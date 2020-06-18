e0df1cadf4bc616613e7fd5a591dbdc330561849
nguyenl1 <lynnthuynguyen@yahoo.com>
2020-06-17 21:22:47 -0700

    got specific details to show, but it's showing the first location on the list instead of a specific location based off its ID

--- local_app/urls.py
+++ local_app/urls.py
@@ -1,6 +1,34 @@
 from django.urls import path
 from . import views
+import requests
+import json
+from .config import get_my_key
+
+#define the api, endpoint, and header
+API_KEY = get_my_key()
+ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
+HEADERS = {'Authorization':'bearer %s' % API_KEY}
+
+PARAMETERS = {
+    'term':'food',
+    'location': 'San Francisco',
+}
+
+#make a request to the yelp APi
+
+response = requests.get(url = ENDPOINT, params= PARAMETERS, headers= HEADERS)
+
+#convert json string to a dictionary 
+
+business_data = response.json()
+
+a = business_data['businesses']
+
+slug = a[3] 
+
+
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('<slug:id>/', views.location_details, name="location_details"),
 ]
--- local_app/views.py
+++ local_app/views.py
@@ -1,5 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+# from .models import 
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+import requests
+import json
+from .config import get_my_key
 
 def home(request):
+    return render(request, "local_app/index.html")
+
+def location_details(request,id):
+    api_key = get_my_key()
+    endpoint = 'https://api.yelp.com/v3/businesses/search'
+    headers = {'Authorization':'bearer %s' % api_key}
+
+    parameters = {
+        'term':'food',
+        'location': 'San Francisco',
+    }
+
+    response = requests.get(url = endpoint, params= parameters, headers= headers)
+
+    business_data = json.loads(response.text)
+
+    businesses = business_data["businesses"]
+
+    for bus in businesses:
+        id = bus["id"]
+        url = f"https://api.yelp.com/v3/businesses/{id}"
+        req = requests.get(url, headers=headers)
+        parsed = json.loads(req.text)
+
+        context = {
+            'parsed':parsed
+        }
+
+        return render(request,'local_app/location_details.html', context=context)
+        
     return render(request, "local_app/index.html")
     
